eb9ad05f96ef37c22f772c57e7a3dfc45a91c43e
Jacob <jwbolling001@gmail.com>
2025-01-08 13:10:56 -0600

    migrated storage schema

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # counting but it gets harder and harder
 
-<a href=""><button>Invite me to your server</button></a>
+<a href="https://discord.com/oauth2/authorize?&client_id=1326208864814633093&scope=bot+applications.commands&permissions=8"><button>Invite me to your server</button></a>
 
 this was a discord bot inspired by the popular "counting" where people count numbers solely for the sake of "number go up"
 
--- bot.py
+++ bot.py
@@ -9,23 +9,22 @@ import os
 # Load environment variables from the .env file
 load_dotenv()
 
-f = open("storage.json", "r")
-count_info = json.loads(f.read())
-g = open("users.json", "r")
-user_info = json.loads(g.read())
-g.close()
+f = open("data/_servers.json", "r")
+server_info = json.loads(f.read())
 f.close()
-
+count_info = {}
+for id in server_info.values():
+    file = open(f'data/{id}.json', "r")
+    count_info[str(id)] = json.loads(file.read())
+    file.close()
+#print(count_info)
 # initialize local vars
 user_cooldowns = {}
 
-# dump function dumps local vars to saved storage.json file
-def dump():
-    file = open("storage.json", "w")
-    file.write(json.dumps(count_info))
-    file.close()
-    file = open("users.json", "w")
-    file.write(json.dumps(user_info))
+# dump function dumps local vars to saved storage json file
+def dump(id):
+    file = open(f'data/{id}.json', "w")
+    file.write(json.dumps(count_info[id]))
     file.close()
 
 # discord perms stuff
@@ -38,25 +37,42 @@ client = discord.Client(intents=intents, activity=activity)
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-    print(f'Current count: {count_info["current"]}')
+    for x in count_info:
+        print(f'Current count in server {x} is {count_info[x]["current"]}')
+    
 
+@client.event
+async def on_guild_join(guild: discord.Guild):
+    # Code to run when the bot joins a server
+    print(f"Joined a new server: {guild.name}")
+    count_info[str(guild.id)] = {
+        "current": 0, "high score": 0, "highest counter": "nobody yet!", "last user": "", "channel": 0
+        }
+    #print(count_info)
+    dump(guild.id)
+
+    
 async def wrong(author, message, reason=None):
+    SERVER = str(message.guild.id)
     if reason == None:
-        await message.channel.send(f'Wrong count, expected {count_info["current"] + 1}. Next count is 1')
+        await message.channel.send(f'Wrong count, expected {count_info[SERVER]["current"] + 1}. Next count is 1')
     else:
         await message.channel.send(reason +  ' Next count is 1')
     await message.add_reaction("❌")
-    count_info["current"] = 0
-    count_info["last user"] = ""
-    user_info[author]["failed"] += 1
-    user_info[author]["slowmode"] *= 2
+    count_info[SERVER]["current"] = 0
+    count_info[SERVER]["last user"] = ""
+    count_info["userdata"][author]["failed"] += 1
+    count_info["userdata"]["slowmode"] *= 2
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+    SERVER = str(message.guild.id)
     m = message.content.split(" ")
     author = str(message.author)
     # test commands
+    if m[0] == ('$guildid'):
+        await message.channel.send(message.guild.id)
     if m[0] == ('$ping'):
         await message.channel.send('Pong!')
     # help command
@@ -71,16 +87,18 @@ By the way, you can't count twice in a row. And try not to fail, because failing
 \- $highscore: outputs current highscore
 \- $count or $currentcount: outputs current count info
 \- $user: use this to find out stats of a user (defaults to current user) e.g. `$user computingsquid`
+\- $slowmode: find out the slowmode of a user (defaults to current user) e.g. `$slowmode computingsquid`
 ### Admin-only commands:
 \- $setchannel: sets current channel to counting channel
 
         """)
     # high score
     if m[0] ==('$highscore'):
-        await message.channel.send(f'Server high score is: {count_info["high score"]}, counted by {count_info["highest counter"]}')
+        await message.channel.send(f'Server high score is: {count_info[SERVER]["high score"]}, counted by {count_info[SERVER]["highest counter"]}')
     if m[0] ==('$currentcount') or m[0] ==('$count'):
-        await message.channel.send(f'The current count is {count_info["current"]}, counted by {count_info["last user"]}')
-    
+        await message.channel.send(f'The current count is {count_info[SERVER]["current"]}, counted by {count_info[SERVER]["last user"]}')
+    if m[0] == '$leaderboard':
+        await message.channel.send(f'Server leaderboard:')
     ##############
     # USER STUFF #
     ##############
@@ -92,7 +110,7 @@ By the way, you can't count twice in a row. And try not to fail, because failing
 
         await message.channel.send(f'fetching user stats for {user}')
         try:
-            user_stats = user_info[user]
+            user_stats = count_info[SERVER]["userdata"][user]
             await message.channel.send(f'Data: \nTotal counts: {user_stats["counts"]} \nFailed counts: {user_stats["failed"]}\nSlowmode: {user_stats["slowmode"]}s')
         except KeyError:
             await message.channel.send(f'ERROR: User {user} not registered')
@@ -102,7 +120,7 @@ By the way, you can't count twice in a row. And try not to fail, because failing
             user = m[2]
             if message.author.guild_permissions.administrator or author == "computingsquid":
                 try:
-                    user_info[user]["slowmode"] = int(m[3])
+                    count_info[SERVER]["userdata"][user]["slowmode"] = int(m[3])
                     await message.channel.send(f'Successfully set {user}\'s slowmode to {m[3]}s')
                 except:
                     await message.channel.send("invalid slowmode passed")
@@ -119,7 +137,7 @@ By the way, you can't count twice in a row. And try not to fail, because failing
             else:
                 user = m[1]
             try:
-                user_stats = user_info[user]
+                user_stats = count_info[SERVER]["userdata"][user]
                 await message.channel.send(f'Current slowmode for {user} is: {user_stats["slowmode"]}s')
             except KeyError:
                 await message.channel.send(f'ERROR: User {user} not registered')
@@ -127,15 +145,15 @@ By the way, you can't count twice in a row. And try not to fail, because failing
     if message.author.guild_permissions.administrator:
         if m[0] == ('$setchannel'):
             await message.channel.send(f'counting channel set to: <#{message.channel.id}>')
-            count_info["channel"] = int(message.channel.id)
+            count_info[SERVER]["channel"] = int(message.channel.id)
         #if m[0] ==('$addadmin'):
         #    username = m[1]
-        #    if not username in count_info["admins"]:
-        #        count_info["admins"].append(username)
+        #    if not username in count_info[SERVER]["admins"]:
+        #        count_info[SERVER]["admins"].append(username)
         #        await message.channel.send(f'set {username} as admin')
         #    else:
         #        await message.channel.send(f'{username} is already an admin')
-        dump()
+        dump(SERVER)
  
     # funny
     if m[0].lower().startswith('is the admin allowed to'):
@@ -153,12 +171,12 @@ By the way, you can't count twice in a row. And try not to fail, because failing
             print(f'falling back to evaluating first block, {number}')
         except: pass
         pass
-    if int(message.channel.id) == count_info["channel"] and isinstance(number, int):
+    if int(message.channel.id) == count_info[SERVER]["channel"] and isinstance(number, int):
         # set number
         #number = eval(m[0])
         # register user
-        if not author in user_info.keys():
-            user_info[author] = {"counts": 0, "slowmode": 1, "failed": 0}
+        if not author in count_info[SERVER]["userdata"].keys():
+            count_info[SERVER]["userdata"][author] = {"counts": 0, "slowmode": 1, "failed": 0}
         
         # check for slowmode
         now = datetime.now()
@@ -166,7 +184,7 @@ By the way, you can't count twice in a row. And try not to fail, because failing
         # user is under cooldown
         if author in user_cooldowns and not author == "computingsquid":
             last_message_time = user_cooldowns[author]
-            cooldown_end = last_message_time + timedelta(seconds=user_info[author]["slowmode"])
+            cooldown_end = last_message_time + timedelta(seconds=count_info[SERVER]["userdata"][author]["slowmode"])
             if now < cooldown_end:
                 # Message sent too soon, delete it
                 await message.delete()
@@ -182,29 +200,29 @@ By the way, you can't count twice in a row. And try not to fail, because failing
         user_cooldowns[author] = now
     
 
-        if count_info["last user"] == author and count_info["last user"] != "computingsquid":
-            print(f'last user counted: {count_info["last user"]} user counted: {author}')
+        if count_info[SERVER]["last user"] == author and count_info[SERVER]["last user"] != "computingsquid":
+            print(f'last user counted: {count_info[SERVER]["last user"]} user counted: {author}')
             await wrong(author, message, "You can't count twice in a row!")
-        elif number == count_info["current"] + 1:
-            #if count_info["last user"] == "computingsquid": await message.channel.send('admin is allowed to count consecutively')
-            count_info["current"] += 1
-            user_info[author]["counts"] += 1
-            count_info["last user"] = author
-            if count_info["high score"] < count_info["current"]:
-                count_info["high score"] = count_info["current"]
-                count_info["highest counter"] = count_info["last user"]
+        elif number == count_info[SERVER]["current"] + 1:
+            #if count_info[SERVER]["last user"] == "computingsquid": await message.channel.send('admin is allowed to count consecutively')
+            count_info[SERVER]["current"] += 1
+            count_info[SERVER]["userdata"][author]["counts"] += 1
+            count_info[SERVER]["last user"] = author
+            if count_info[SERVER]["high score"] < count_info[SERVER]["current"]:
+                count_info[SERVER]["high score"] = count_info[SERVER]["current"]
+                count_info[SERVER]["highest counter"] = count_info[SERVER]["last user"]
             await message.add_reaction("✅")
             if number == 69:
                 await message.add_reaction("🇳")
                 await message.add_reaction("🇮")
                 await message.add_reaction("🇨")
                 await message.add_reaction("🇪")
-        elif count_info["current"] == 0:
+        elif count_info[SERVER]["current"] == 0:
             await message.add_reaction("⚠️")
             await message.channel.send('the counting starts at 1!')
         else:
             await wrong(author, message)
-        dump()
+        dump(SERVER)
 
 
 client.run(os.getenv('TOKEN'))
